60fbfa0e903cf73b22e8d5820a49a5ce8a91efae
chekofif <gidi@gidi.io>
2018-12-26 23:08:16 +0000

    Added basic CLI support and identity match

---
+++ src/filtering/identity.rs
@@ -0,0 +1,52 @@
+use json::JsonValue;
+use json::object;
+
+pub fn identity(input: Option<&JsonValue>) -> Option<&JsonValue> {
+  match input {
+    Some(_) => input,
+    _ => None
+  }
+}
+
+pub fn matches(pattern: &str) -> bool {
+  match pattern {
+    "." => true,
+    _ => false
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn should_match_dot() {
+    assert_eq!(matches("."), true);
+  }
+
+  #[test]
+  fn shouldnt_match_anything_else() {
+    assert_eq!(matches(".prop"), false);
+  }
+
+  #[test]
+  fn should_return_none_when_json_isnt_present() {
+    assert_eq!(identity(None), None);
+  }
+
+  #[test]
+  fn should_return_some_json_when_json_is_present() {
+    let ref data = object!{
+        "name"    => "John Doe",
+        "age"     => 30
+    };
+
+    assert_eq!(
+      match identity(Some(data)){
+        Some(d) => d,
+        None => &json::Null
+      },
+      data
+    );
+  }
+}
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+extern crate clap;
+
+use clap::{Arg, App};
+use std::io::{self, BufRead};
+
+mod filtering;
+
+fn print_input(filter: &str) {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        if filtering::identity::matches(filter) {
+            let line = line.expect("Could not read line from standard in");
+            println!(" - {}", line);
+        }
+    }
+}
+
+fn main() {
+    let matches = App::new("jqr")
+        .version("0.0.1")
+        .author("Gidi Meir Morris <gidi@gidi.io>")
+        .about("jqr is a tool for processing JSON input")
+        .arg(
+            Arg::with_name("filter")
+                 .required(true)
+                 .takes_value(true)
+                 .index(1)
+                 .help("JSON query filter")
+        )
+        .get_matches();
+    
+    let filter = matches.value_of("filter").unwrap();
+    
+    println!("filter: {}", filter);
+
+    print_input(filter);
+}
