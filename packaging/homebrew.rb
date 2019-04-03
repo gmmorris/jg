e5fddeec6843619e7403f4c196e2e051e0dca69b
chekofif <gidi@gidi.io>
2019-04-03 08:37:55 +0100

    feat(homebrew): Updated version in homebrew

--- packaging/homebrew.rb
+++ packaging/homebrew.rb
@@ -1,19 +1,18 @@
 class Jg < Formula
+  version '0.1.3'
   desc "A command-line JSON processor in Rust. jg searches for selector patterns in json input, jg prints each json object that matches a pattern."
   homepage "https://github.com/gmmorris/jg"
-  url "https://github.com/gmmorris/jg/archive/0.1.3.osx.tar.gz"
-  sha256 "e78ba9e05d8a44d81422b02100f0105c1b24423393b2c68783a0d703859d9caf"
-  # depends_on "cmake" => :build
+
+  if OS.mac?
+      url "https://github.com/gmmorris/jg/releases/download/#{version}/jg-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "4f36a8cebfd51cf97d99f415910c09e4354f847fde56bdc3e55311872aad0ed9"
+  elsif OS.linux?
+      url "https://github.com/gmmorris/jg/releases/download/#{version}/jg-#{version}-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "d10bd0cad1cf5a0e4c8ef26f2d531f41638bde71b6e499555815da88e73a5fa5"
+  end
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    bin.install "jg"
   end
 
   test do
