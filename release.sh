607ed105e31a3287dd6116b0f8d46aecb7867c72
Gidi Meir Morris <github@gidi.io>
2022-06-08 13:00:51 +0100

    updated release

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
-export TRAVIS_TAG=`cargo pkgid | cut -d# -f2 | cut -d: -f2`
-git tag "$TRAVIS_TAG"
+export RELEASE_TAG=`cargo pkgid | cut -d# -f2 | cut -d: -f2`
+git tag "v$RELEASE_TAG"
 git push --tags
